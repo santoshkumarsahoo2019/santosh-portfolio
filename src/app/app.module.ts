86036113bd93aaa538304422adb3236e8c33bb72
santosh.kumar.sahoo <santoshassassin209@gmail.com>
2022-10-24 18:11:24 +0530

    blog app integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog-application/blog-application.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   imports: [
---
+++ src/app/blog-application/blog-application.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : 'blog',
+    templateUrl: './blog-application.component.html'
+})
+
+export class BlogComponent {}
