048bfd34c3f6ce8d6514631f7f8e0cc57b957062
santosh.kumar.sahoo <santoshassassin209@gmail.com>
2022-10-25 14:21:52 +0530

    Modularized the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,25 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { SidebarComponent } from './shared/component/sidebar/sidebar.component';
+import { SharedModule } from './modules/shared/shared.module';
+import { SidebarComponent } from './modules/shared/component/sidebar/sidebar.component';
+import { HomeComponent } from './modules/home/home.component';
+import { AboutComponent } from './modules/about/about.component';
+import { AboutModule } from './modules/about/about.module';
+import { HomeModule } from './modules/home/home.module';
+import { ExperienceModule } from './modules/experience/experience.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SidebarComponent
+    AppComponent
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AboutModule,
+    HomeModule,
+    ExperienceModule,
     SharedModule
   ],
 
