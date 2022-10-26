59e287e6d578b79dde8e118faf9915cea6439447
santosh.kumar.sahoo <santoshassassin209@gmail.com>
2022-10-26 22:04:57 +0530

    Sidebar routing

--- src/app/modules/shared/component-config/sidebar-config.ts
+++ src/app/modules/shared/component-config/sidebar-config.ts
@@ -6,31 +6,37 @@ export class SidebarConfiguration {
     sidebarTabs = [
         {
             href : '',
+            route : './about',
             icon : this.sidebarIcon.aboutMe,
             tabName : 'About Me'
         },
         {
             href : '',
+            route : './experience',
             icon : this.sidebarIcon.workExperience,
             tabName : 'Work Experience'
         },
         {
             href : '',
+            route : './education',
             icon : this.sidebarIcon.education,
             tabName : 'Education'
         },
         {
             href : '',
+            route : './achievement',
             icon : this.sidebarIcon.achievement,
             tabName : 'Achievement'
         },
         {
             href : '',
+            route : './blog',
             icon : this.sidebarIcon.blog,
             tabName : 'Blog'
         },
         {
             href : '',
+            route : './home',
             icon : this.sidebarIcon.home,
             tabName : 'Home'
         }
