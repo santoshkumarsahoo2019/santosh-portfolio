a88e8610cee45e99552f8874a53067ad8db6ce3f
Santosh Kumar Sahoo <33922110+SantosAssassin@users.noreply.github.com>
2022-10-28 15:10:54 +0530

    Scalable SCSS structure and Theming (#8)

--- src/app/modules/shared/component/sidebar/sidebar.component.ts
+++ src/app/modules/shared/component/sidebar/sidebar.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector : "sidebar",
-    templateUrl : './sidebar.component.html'
+    templateUrl : './sidebar.component.html',
+    styleUrls: ['./sidebar.component.scss']
 })
 
 export class SidebarComponent {
